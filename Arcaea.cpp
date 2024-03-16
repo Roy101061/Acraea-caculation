784555cfc1bb05e236985125d02992b0aede20aa
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-03-17 01:17:23 +0800

    add read r30/b30/b30info DB

--- Arcaea.cpp
+++ Arcaea.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#define LevelCount 5 //4+1 for blank
+#define LevelCount 6 //5+1 for blank
+ifstream in;
+ofstream out;
 
 using namespace std;
+
+void readfail(){
+    if(in.fail()){
+        cout<<"R30 input fail!";
+        exit(1);
+    }
+}
 
 int main(){
     string LevelNames[LevelCount], SongList[500];
     float DB[500][LevelCount];
-    int i=2;  //start from 2 due to spreadsheet 
+    int i=1;  //start from 2(1) due to spreadsheet 
+    int B30info[LevelCount];
 
-    ifstream in;
-    ofstream out;
+    //song DB
     in.open("Arcaea Potential Cal.csv");
-    if(in.fail()){
-        cout<<"DB input fail!";
-        exit(1);
-    }
+    readfail();
     for(int i=0; i<LevelCount; i++){
         in>>LevelNames[i];
     }
@@ -26,11 +32,9 @@ int main(){
         }
     }
 
+    //R30 DB
     in.open("R30.csv");
-    if(in.fail()){
-        cout<<"R30 input fail!";
-        exit(1);
-    }
+    readfail();
     deque<string [2]> R30Song;
     deque<float [6]> R30Rec;
     while(!in.eof()){
@@ -46,6 +50,29 @@ int main(){
         R30Rec.push_back(numtemp);
     }
 
+    //B30 DB
+    in.open("B30.csv");
+    readfail();
+    deque<string [2]> B30Song;
+    deque<float [6]> B30Rec;
+    while(!in.eof()){
+        string nametemp[2];
+        float numtemp[6];
+
+        for(int n=0; n<2; n++)
+            in>>nametemp[n];
+        B30Song.push_back(nametemp);
+
+        for(int n=0; n<6; n++)
+            in>>numtemp[n];
+        B30Rec.push_back(numtemp);
+    }
+
+    //B30info
+    in.open("B30info.csv");
+    readfail();
+    for(int i=1; i<LevelCount; i++)
+        in>>B30info[i];
 
 
     return 0;
