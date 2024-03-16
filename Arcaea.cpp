6cd75c825580811783361d6953921277c18045bb
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-03-16 19:21:43 +0800

    create R30 with ex. + code

--- Arcaea.cpp
+++ Arcaea.cpp
@@ -1,9 +1,48 @@
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
+#define LevelCount 5 //4+1 for blank
 
 using namespace std;
 
 int main(){
+    string LevelNames[LevelCount], SongList[500];
+    float DB[500][LevelCount];
+    int i=2;  //start from 2 due to spreadsheet 
+
+    ifstream in;
+    ofstream out;
+    in.open("Arcaea Potential Cal.csv");
+    if(in.fail()){
+        cout<<"DB input fail!";
+        exit(1);
+    }
+    for(int i=0; i<LevelCount; i++){
+        in>>LevelNames[i];
+    }
+    while(!in.eof()){
+        i++;
+        in>>SongList[i];
+        for(int n=1; n<LevelCount; n++){
+            in>>DB[i][n];
+        }
+    }
+
+    in.open("R30.csv");
+    if(in.fail()){
+        cout<<"R30 input fail!";
+        exit(1);
+    }
+    deque<string [2]> R30Song;
+    deque<float [6]> R30Rec;
+    while(!in.eof()){
+        string nametemp;
+        int numtemp;
+        for(int n=0; n<2; n++){
+            in>>nametemp;
+            R30Song.push_back(nametemp);
+        }
+    }
+
+
 
     return 0;
 }
