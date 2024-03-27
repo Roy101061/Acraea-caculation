9c946df468fab43bf916fe00127524c0404082c2
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-03-27 15:14:39 +0800

    Debug

--- Arcaea.cpp
+++ Arcaea.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <array>
 #define LevelCount 6 //5+1 for blank
 
 using namespace std;
@@ -9,10 +10,10 @@ string LevelNames[LevelCount], SongList[500];
 float DB[500][LevelCount];
 int B30info[LevelCount];
 
-void readfail(){
+void readfail(int n){
     if(in.fail()){
-        cout<<"R30 input fail!";
-        exit(1);
+        cout<<"Input fail!";
+        exit(n);
     }
 }
 
@@ -21,7 +22,7 @@ void setupDB(){
     
     //song DB
     in.open("Arcaea Potential Cal.csv");
-    readfail();
+    readfail(1);
     for(int i=0; i<LevelCount; i++){
         in>>LevelNames[i];
     }
@@ -31,50 +32,56 @@ void setupDB(){
         for(int n=1; n<LevelCount; n++){
             in>>DB[i][n];
         }
+        cout<<i<<endl;
     }
+	in.close();
 
     //R30 DB
     in.open("R30.csv");
-    readfail();
-    deque<string [2]> R30Song;
-    deque<float [6]> R30Rec;
+    readfail(2);
+    deque<pair <string,string> > R30Song;
+    deque<array <float, 6> > R30Rec;
     while(!in.eof()){
-        string nametemp[2];
-        float numtemp[6];
+        pair<string,string> nametemp;
+        array<float, 6> numtemp;
 
-        for(int n=0; n<2; n++)
-            in>>nametemp[n];
+        in>>nametemp.first;
+        in>>nametemp.second;
         R30Song.push_back(nametemp);
 
         for(int n=0; n<6; n++)
             in>>numtemp[n];
         R30Rec.push_back(numtemp);
     }
+	in.close();
 
     //B30 DB
     in.open("B30.csv");
-    readfail();
-    deque<string [2]> B30Song;
-    deque<float [6]> B30Rec;
+    readfail(3);
+    deque<pair <string,string> > B30Song;
+    deque<array <float, 6> > B30Rec;
     while(!in.eof()){
-        string nametemp[2];
-        float numtemp[6];
+        pair<string,string> nametemp;
+        array<float, 6> numtemp;
 
-        for(int n=0; n<2; n++)
-            in>>nametemp[n];
+        in>>nametemp.first;
+        in>>nametemp.second;
         B30Song.push_back(nametemp);
 
         for(int n=0; n<6; n++)
             in>>numtemp[n];
         B30Rec.push_back(numtemp);
     }
+    in.close();
 
     //B30info
     in.open("B30info.csv");
-    readfail();
+    readfail(4);
     for(int i=1; i<LevelCount; i++)
         in>>B30info[i];
+    in.close();
 
+    cout<<"DB read success!";
 }
 
 int main(){
