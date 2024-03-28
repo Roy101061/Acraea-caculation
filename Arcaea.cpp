689b05870d0058908dc693e2a5f136c9a8ff2041
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-03-28 16:25:09 +0800

    fix DB bug

--- Arcaea.cpp
+++ Arcaea.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 #include <array>
-#define LevelCount 6 //5+1 for blank
+#define LevelCount 5+1 //+1 for blank
+#define SongCount 378+2 //+2 to sync with spreadsheet
 
 using namespace std;
 
 ifstream in;
 ofstream out;
-string LevelNames[LevelCount], SongList[500], DBtemp;
-float DB[500][LevelCount];
-int B30info[LevelCount];
+string LevelNames[LevelCount], SongList[500], Stringtemp;
+float DB[SongCount][LevelCount];
+int B30info[LevelCount+1];
 
 void readfail(int n){
     if(in.fail()){
@@ -27,75 +28,84 @@ void setupDB(){
     }
 
     int i=1;  //start from 2(1) due to spreadsheet
-    while(!in.eof()){
+    while(i<SongCount){
         i++;
         getline(in, SongList[i], ',');
         for(int n=1; n<LevelCount; n++){
-            getline(in, DBtemp, ',');
-            DB[i][n]=stof(DBtemp);
+            getline(in, Stringtemp, ',');
+            DB[i][n]=stof(Stringtemp);
         }
     }
 	in.close();
+    
+    //B30info
+    in.open("B30info.csv");
+    readfail(2);
+    for(int i=1; i<LevelCount+1; i++){
+        getline(in, Stringtemp, ',');
+        B30info[i]=stof(Stringtemp);
+    }
+    in.close();
+
+    cout<<endl<<"DB read success!"<<endl;
 
     //R30 DB
     in.open("R30.csv");
-    readfail(2);
+    readfail(3);
     deque<pair <string,string> > R30Song;
     deque<array <float, 6> > R30Rec;
     while(!in.eof()){
         pair<string,string> nametemp;
         array<float, 6> numtemp;
 
-        in>>nametemp.first;
-        in>>nametemp.second;
+        getline(in, nametemp.first, ',');
+        getline(in, nametemp.second, ',');
         R30Song.push_back(nametemp);
 
-        for(int n=0; n<6; n++)
-            in>>numtemp[n];
+        for(int n=0; n<6; n++){
+            getline(in, Stringtemp, ',');
+            numtemp[n]=stof(Stringtemp);
+        }
         R30Rec.push_back(numtemp);
     }
 	in.close();
 
     //B30 DB
     in.open("B30.csv");
-    readfail(3);
+    readfail(4);
     deque<pair <string,string> > B30Song;
     deque<array <float, 6> > B30Rec;
-    while(!in.eof()){
+    i=0;
+    while(i<B30info[6]){
         pair<string,string> nametemp;
         array<float, 6> numtemp;
 
-        in>>nametemp.first;
-        in>>nametemp.second;
+        getline(in, nametemp.first, ',');
+        getline(in, nametemp.second, ',');
         B30Song.push_back(nametemp);
 
-        for(int n=0; n<6; n++)
-            in>>numtemp[n];
+        for(int n=0; n<6; n++){
+            getline(in, Stringtemp, ',');
+            numtemp[n]=stof(Stringtemp);
+        }
         B30Rec.push_back(numtemp);
+        i++;
     }
     in.close();
 
-    //B30info
-    in.open("B30info.csv");
-    readfail(4);
-    for(int i=1; i<LevelCount; i++)
-        in>>B30info[i];
-    in.close();
-
-    cout<<endl<<"DB read success!"<<endl;
 }
 
 int main(){
     setupDB();
-    string song,level;
-    int userdata[5];
+    string Song,Level;
+    int Userdata[5], SongNum, LevelNum;
 
     //UI
     cout<<"Enter your score:"<<endl<<"(song,level,score,MaxPureCount,PureCount,FarCount,LostCount)"<<endl;
-    cin>>song>>level;
+    cin>>Song>>Level;
     for(int i=0; i<5; i++)
-        cin>>userdata[i];
-    
+        cin>>Userdata[i];
+    for(SongNum=2; ; )
 
     return 0;
 }
