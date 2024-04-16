87dfc15ef5a47f3d5029b70b987b6e8cfc208283
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-04-16 20:29:07 +0800

    finish B30/R10 update (w/o input file update)

--- Arcaea.cpp
+++ Arcaea.cpp
@@ -10,6 +10,8 @@ ofstream out;
 string LevelNames[LevelCount], SongList[500], Stringtemp;
 float DB[SongCount][LevelCount];
 int B30info[LevelCount+1];
+deque<pair <string,string> > R30Song, B30Song;
+deque<array <float, 6> > R30Rec, B30Rec;
 
 void readfail(int n){
     if(in.fail()){
@@ -53,8 +55,6 @@ void setupDB(){
     //R30 DB
     in.open("R30.csv");
     readfail(3);
-    deque<pair <string,string> > R30Song;
-    deque<array <float, 6> > R30Rec;
     while(!in.eof()){
         pair<string,string> nametemp;
         array<float, 6> numtemp;
@@ -75,8 +75,6 @@ void setupDB(){
     //B30 DB
     in.open("B30.csv");
     readfail(4);
-    deque<pair <string,string> > B30Song;
-    deque<array <float, 6> > B30Rec;
     i=0;
     while(i<B30info[6]){
         pair<string,string> nametemp;
@@ -101,8 +99,11 @@ void setupDB(){
 int main(){
     setupDB();
     string Song,Level;
-    int Userdata[5], SongNum, LevelNum;
+    int Userdata[5], SongNum, LevelNum, SearchRec, SearchEnd, temp;
     float Diffuculty, ppt;
+    bool recB30, recR10;
+    pair<string, string> addSong;
+    array<float, 6> addRec;
 
     //UI
     cout<<"Enter your score:"<<endl<<"(song,level,score,MaxPureCount,PureCount,FarCount,LostCount,)"<<endl;
@@ -116,6 +117,12 @@ int main(){
     for(SongNum=2; Song!=SongList[SongNum] && SongNum<SongCount; SongNum++);
     for(LevelNum=1; Level!=LevelNames[LevelNum] && LevelNum<LevelCount; LevelNum++);
     Diffuculty=DB[SongNum][LevelNum];
+    for(int i=0; i<6; i++)
+        addRec[i]=Userdata[i];
+    addSong.first=Song;
+    addSong.second=Level;
+
+    
     if(Diffuculty!=0){
         if(Userdata[0]>=10000000){
             ppt=Diffuculty+2;
@@ -126,7 +133,49 @@ int main(){
         else{
             ppt=Diffuculty+((float(Userdata[0])-9500000)/300000);
         }
-        cout<<ppt;
+        cout<<"ppt search result: "<<ppt<<endl<<"Add B30/R10 record?"<<endl;
+        cin>>recB30;
+        cin.ignore();
+        cin>>recR10;
+        if(recB30){
+            SearchRec=0; SearchEnd=0;
+            for(temp=0; temp<LevelNum; temp++){
+                SearchRec+=B30info[temp];
+                SearchEnd+=B30info[temp];
+            }
+            SearchEnd+=B30info[temp];
+
+            for(temp=SearchRec; temp<SearchEnd; temp++){
+                if(B30Song[temp].first==Song)
+                    break;
+            }
+
+            if(temp==SearchEnd){  //no rec found
+                    B30Song.insert(B30Song.begin()+temp, addSong);
+                    B30Rec.insert(B30Rec.begin()+temp, addRec);
+                    cout<<"New record added!"<<endl;
+                }
+                else{
+                    if(B30Rec[temp][0]<=Userdata[0]){
+                        for(int i=0; i<6; i++){
+                            B30Rec[temp][i]=Userdata[i];
+                            cout<<"Record exist! PB updated."<<endl;
+                        }
+                    }
+                    else   
+                        cout<<"Record exist, but not PB."<<endl;
+                }
+
+        }
+        if(recR10){
+            if(R30Rec.size()==30){
+                R30Rec.pop_front();
+                R30Song.pop_front();
+            }
+            R30Rec.push_back(addRec);
+            R30Song.push_back(addSong);
+            cout<<"R10 updated!"<<endl;
+        }
     }
     else{
         cout<<"Can't found Song/Level.";
