efc081cc403219cc33b627350c356955299d9adf
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-05-02 16:22:13 +0800

    test

--- py/cogs/main.py
+++ py/cogs/main.py
@@ -3,25 +3,48 @@ from discord.ext import commands
 from discord import app_commands
 from typing import Optional
 from discord.app_commands import Choice
+import csv
 
-LevelCount = 5+1
-SongCount = 378 
+LevelCount = 5
+SongCount = 378
+ppt = -1
+with open("DB.csv",encoding="utf-8") as csvfile:
+    csvread = csv.reader(csvfile)
+    db = list(csvread)
 
-DB = open("DB.csv")
-
+def calc(rating, score):
+    if(score>=10000000):
+        ppt = rating+2
+    elif(score>=9800000):
+        ppt = rating+1+(score-9800000)/200000
+    else:
+        ppt = rating+(score-9500000)/300000
 
 class Main(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name = "calc", description = "Calculate Potential")
-    @app_commands.describe(score = "Score", song = "Song", diff = "Difficutly", rating = "Song Rating", b30 = "Add to B30 record?", r10 = "Add to R10 record?", pc = "Pure Count", mpc = "Max Pure Count", fc = "Far Count", lc = "Lost Count")
+    @app_commands.command(name = "calc_by_rating", description = "Calculate Potential by Rating")
+    @app_commands.describe(score = "Score", rating = "Song Rating", r10 = "Add to R10 record?")
     @app_commands.choices(
-        song = [
-            
-        ],
+        r10 =[
+            Choice(name = "YES", value = 1),
+            Choice(name = "NO", value = 0)
+        ]
+    )
+    async def calc_by_rating(self, interaction: discord.Interaction, score: int, rating: float, r10: int):
+        calc(rating, score)
+        await interaction.response.send_message(ppt)
+
+    @app_commands.command(name = "calc_by_song", description = "Calculate Potential by Song")
+    @app_commands.describe(score = "Score", song = "Song", diff = "Difficutly", b30 = "Add to B30 record?", r10 = "Add to R10 record?", pc = "Pure Count", mpc = "Max Pure Count", fc = "Far Count", lc = "Lost Count")
+    @app_commands.choices(
         diff = [
-            
+            Choice(name = "PST", value = 1),
+            Choice(name = "PRS", value = 2),
+            Choice(name = "FTR", value = 3),
+            Choice(name = "BYD", value = 4),
+            Choice(name = "ETR/ETN", value = 5)
         ],
         b30 = [
             Choice(name = "YES", value = 1),
@@ -32,8 +55,9 @@ class Main(commands.Cog):
             Choice(name = "NO", value = 0)
         ]
     )
-    async def calc(self, interaction: discord.Interaction, score: int, song: Optional[str], diff: Optional[str], rating: Optional[float], b30: int, r10: int, pc: int, mpc: int, fc: int, lc: int):
-        await interaction.response.send_message("Hello, world!")
+    async def calc_by_song(self, interaction: discord.Interaction, score: int, song: str, diff: int, b30: int, r10: int, pc: Optional[int], mpc: Optional[int], fc: Optional[int], lc: Optional[int]):
+        await interaction.response.send_message(score)
 
+    
 async def setup(bot: commands.Bot):
     await bot.add_cog(Main(bot))
