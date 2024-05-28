5a582fda0ad530d06df5cdebc647c60aac2c6419
Roy101061 <86598941+Roy101061@users.noreply.github.com>
2024-05-28 21:57:51 +0800

    update

--- py/cogs/main.py
+++ py/cogs/main.py
@@ -32,7 +32,7 @@ class Main(commands.Cog):
         await interaction.response.send_message(calc(rating, score))
 
     @app_commands.command(name = "calc_by_song", description = "Calculate Potential by Song")
-    @app_commands.describe(score = "Score", song = "Song", diff = "Difficutly", b30 = "Add to B30 record?", r10 = "Add to R10 record?", pc = "Pure Count", mpc = "Max Pure Count", fc = "Far Count", lc = "Lost Count")
+    @app_commands.describe(score = "Score", song = "Song", diff = "Difficutly", pc = "Pure Count", mpc = "Max Pure Count", fc = "Far Count", lc = "Lost Count")
     @app_commands.choices(
         diff = [
             Choice(name = "PST", value = 1),
@@ -40,18 +40,10 @@ class Main(commands.Cog):
             Choice(name = "FTR", value = 3),
             Choice(name = "BYD", value = 4),
             Choice(name = "ETR/ETN", value = 5)
-        ],
-        b30 = [
-            Choice(name = "YES", value = 1),
-            Choice(name = "NO", value = 0)
-        ],
-        r10 =[
-            Choice(name = "YES", value = 1),
-            Choice(name = "NO", value = 0)
         ]
     )
     
-    async def calc_by_song(self, interaction: discord.Interaction, score: int, song: str, diff: int, b30: int, r10: int, pc: int, mpc: int, fc: int, lc: int):
+    async def calc_by_song(self, interaction: discord.Interaction, score: int, song: str, diff: int, pc: int, mpc: int, fc: int, lc: int):
         user_id = interaction.user.id
         min_distance = float('inf')
         for num in range(0, SongCount-1):
@@ -62,8 +54,19 @@ class Main(commands.Cog):
         if(db[songnum][diff] == 0):
             await interaction.response.send_message("No Song / Difficulty found!")
         else:
-            cacu(score, db[songnum][0], diff, b30, r10, pc, mpc, fc, lc, user_id)
-            await interaction.response.send_message(calc(float(db[songnum][diff]), score))
+            #cacu(score, db[songnum][0], diff, pc, mpc, fc, lc, user_id)
+            embed=discord.Embed(title="Search result", color=0x124cfd)
+            embed.add_field(name="Song", value=db[songnum][0], inline=True)
+            embed.add_field(name="Difficulty", value=db[0][diff], inline=True)
+            embed.add_field(name="Level Potential", value=db[songnum][diff], inline=True)
+            embed.add_field(name="Result", value=calc(float(db[songnum][diff]), score), inline=True)
+            
+            
+            view = discord.ui.View()
+            view.add_item()
+            view.add_item()
+            
+            await interaction.response.send_message(embed=embed, view=view)
             
             
     
